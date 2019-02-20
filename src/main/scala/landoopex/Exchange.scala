1411d373f30b7d9319fb4e5124c12f8c1094063e
Pawel Szulc <paul.szulc@gmail.com>
2019-02-21 00:51:32 +0100

    Add dummy implementation of Exchange
    This should be eventually removed

--- src/main/scala/landoopex/Exchange.scala
+++ src/main/scala/landoopex/Exchange.scala
@@ -16,4 +16,16 @@ trait Exchange[F[_]] {
 object Exchange {
 
   def apply[F[_]: Exchange]: Exchange[F] = implicitly[Exchange[F]]
+
+  // TODO remove this eventually
+  def dummy[F[_]: Applicative] = new Exchange[F] {
+    def convert(
+        amount: Double,
+        from: Currency,
+        to: Currency
+    ): F[ExchangeErr[Result]] =
+      if (amount <= 0) insufficientAmount(amount).asLeft[Result].pure[F]
+      else Result(2.0, amount * 2).asRight[ExchangeError].pure[F]
+  }
+
 }
